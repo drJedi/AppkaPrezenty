3792a1889095874f1ff919c9223af319c50f5bd6
Paweł Wilkosz <47307348+drJedi@users.noreply.github.com>
2025-03-14 09:10:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -152,10 +152,10 @@ document.addEventListener('DOMContentLoaded', () => {
         { src: 'https://cewe-uk.leadfamly.com/files/4649/Gift_Finder_Updates_2023/Question1/q1a1myparents.png', description: 'My parents' },
         { src: 'https://cewe-uk.leadfamly.com/files/4649/Gift_Finder_Updates_2023/Question1/q1a2theyoungestones0-6yearsold.png', description: 'The youngest ones (0-6 years old)' },
         { src: 'https://cewe-uk.leadfamly.com/files/4649/Gift_Finder_Updates_2023/Question1/q1a8theolderchildren.png', description: 'The older children (10-19 years old)' },
-        { src: '/Aplikacja-do-prezetow/imagesApp/dla-drugiej-osoby.png', description: 'My other half' },
+        { src: './imagesApp/dla-drugiej-osoby.png', description: 'My other half' },
         { src: 'https://cewe-uk.leadfamly.com/files/4649/Gift_Finder_Updates_2023/Question1/q1a4auntiesanduncles.png', description: 'Aunties and uncles' },
-        { src: '/Aplikacja-do-prezetow/imagesApp/mkbewe__kapitan_bomba__by_rebus2077_dixzs6w.png', description: 'My brothers or sisters' },
-        { src: '/Aplikacja-do-prezetow/imagesApp/tytus.png', description: 'My friends' },
+        { src: './imagesApp/mkbewe__kapitan_bomba__by_rebus2077_dixzs6w.png', description: 'My brothers or sisters' },
+        { src: './imagesApp/tytus.png', description: 'My friends' },
         { src: 'https://cewe-uk.leadfamly.com/files/4649/Gift_Finder_Updates_2023/Question1/q1a6thegrandparents.png', description: 'The grandparents' }
       ];
   
